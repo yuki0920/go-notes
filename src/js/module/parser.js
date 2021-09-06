898e7785a828e63c472377d94afb64219000db84
yuki0920 <ywsep20@gmail.com>
2021-09-06 11:47:16 +0900

    Add preview layout

--- src/js/module/parser.js
+++ src/js/module/parser.js
@@ -5,4 +5,8 @@ const { Remarkable } = window.remarkable;
 const md = new Remarkable({
   html: true,
   breaks: true,
+  langPrefix: 'language-',
+  highlight: function(str, lang) {
+    return Prism.highlight(str, Prism.languages[lang], lang);
+  }
 });
