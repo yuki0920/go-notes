c8b818540894051d427ae473054d820a3757dff6
yuki0920 <ywsep20@gmail.com>
2021-09-19 17:19:25 +0900

    Fixex footer for bottom

---
+++ front/components/FooterBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <div class="footer">
+      <ul class="footer__menu">
+        <li class="footer__menu-item">
+          <a href="https://twitter.com/YukiWebTech">Twitter</a>
+        </li>
+        <li class="footer__menu-item">
+          <a href="https://github.com/yuki0920">GitHub</a>
+        </li>
+      </ul>
+      <div class="footer__copy-right">
+        © 2021 Go Notes
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ front/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+    <a class="header__logo" href="/">Go Notes</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  name: 'Nav'
+})
+</script>
