43c03f891b6f0818c0621eefb435525dce4d65b3
yuki0920 <ywsep20@gmail.com>
2021-09-24 16:45:38 +0900

    Add test without cookie

--- api/server/server_test.go
+++ api/server/server_test.go
@@ -1,26 +1,15 @@
 package server
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
-	"yuki0920/go-blog/util"
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
-
-func setDummyCookie(c echo.Context) error {
-	cookie := &http.Cookie{}
-	cookie.Name = "jwt"
-	dummyToken, _ := util.GenerateJwtToken("dummy")
-	cookie.Value = dummyToken
-	cookie.Expires = time.Now().Add(24 * time.Hour)
-	c.SetCookie(cookie)
-	return nil
-}
 
 func TestSample(t *testing.T) {
 	e := echo.New()
@@ -49,6 +38,100 @@ func TestSample(t *testing.T) {
 	assert.Equal(t, http.StatusOK, res.StatusCode)
 	assert.JSONEq(t, articleJSON, string(body))
 }
+
+func TestGetAuthWithoutCookie(t *testing.T) {
+	e := echo.New()
+	ts := httptest.NewServer(Router(e))
+	defer ts.Close()
+
+	req, _ := http.NewRequest("GET", ts.URL+"/api/auth", nil)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("http.Put failed: %s", err)
+	}
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Fatalf("ioutil.ReadAll body failed: %s", err)
+	}
+
+	authJSON := `{"IsAuthenticated":false}`
+
+	assert.JSONEq(t, authJSON, string(body))
+}
+
+func TestCreateArticleWithoutCookie(t *testing.T) {
+	e := echo.New()
+	ts := httptest.NewServer(Router(e))
+	defer ts.Close()
+
+	jsonStr := `{"title":"タイトル","body":"ボディ"}`
+	// http.NewRequestのの第3引数にはio.Readerを指定するため、バイト列を渡す
+	paramsJson := bytes.NewBuffer([]byte(jsonStr))
+	req, _ := http.NewRequest("POST", ts.URL+"/api/articles", paramsJson)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("http.Put failed: %s", err)
+	}
+
+	assert.Equal(t, http.StatusUnauthorized, res.StatusCode)
+}
+
+func TestUpdateArticleWithoutCookie(t *testing.T) {
+	e := echo.New()
+	ts := httptest.NewServer(Router(e))
+	defer ts.Close()
+
+	jsonStr := `{"title":"タイトル","body":"ボディ"}`
+	// http.NewRequestのの第3引数にはio.Readerを指定するため、バイト列を渡す
+	paramsJson := bytes.NewBuffer([]byte(jsonStr))
+	req, _ := http.NewRequest("PUT", ts.URL+"/api/articles/1", paramsJson)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("http.Put failed: %s", err)
+	}
+
+	assert.Equal(t, http.StatusUnauthorized, res.StatusCode)
+}
+
+func TestDeleteArticleWithoutCookie(t *testing.T) {
+	e := echo.New()
+	ts := httptest.NewServer(Router(e))
+	defer ts.Close()
+
+	req, _ := http.NewRequest("DELETE", ts.URL+"/api/articles/1", nil)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("http.Put failed: %s", err)
+	}
+
+	assert.Equal(t, http.StatusUnauthorized, res.StatusCode)
+}
+
+// func setDummyCookie(c echo.Context) error {
+// 	cookie := &http.Cookie{}
+// 	cookie.Name = "jwt"
+// 	dummyToken, _ := util.GenerateJwtToken("dummy")
+// 	cookie.Value = dummyToken
+// 	cookie.Expires = time.Now().Add(24 * time.Hour)
+// 	c.SetCookie(cookie)
+// 	return nil
+// }
 
 // func TestPutUnknownType(t *testing.T) {
 // 	e := echo.New()
