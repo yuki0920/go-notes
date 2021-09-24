d793031400bc14de5ce96ddf319478c5d867c1f2
yuki0920 <ywsep20@gmail.com>
2021-09-24 17:17:10 +0900

    Add tests with dummy cookie

--- api/server/server_test.go
+++ api/server/server_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/go-playground/validator.v9"
 )
 
 func TestSample(t *testing.T) {
@@ -66,6 +67,34 @@ func TestGetAuthWithoutCookie(t *testing.T) {
 	assert.JSONEq(t, authJSON, string(body))
 }
 
+func TestGetAuthWithCookie(t *testing.T) {
+	e := echo.New()
+	ts := httptest.NewServer(Router(e))
+	defer ts.Close()
+
+	req, _ := http.NewRequest("GET", ts.URL+"/api/auth", nil)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzI3Mjk3NjEsImlzcyI6InVzZXIifQ.9NlQvzKYioB2um6NvB_ZpdKf9og5nRDb9oUzNAjkohk;")
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("http.Put failed: %s", err)
+	}
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Fatalf("ioutil.ReadAll body failed: %s", err)
+	}
+
+	authJSON := `{"IsAuthenticated":true}`
+
+	assert.JSONEq(t, authJSON, string(body))
+}
+
 func TestCreateArticleWithoutCookie(t *testing.T) {
 	e := echo.New()
 	ts := httptest.NewServer(Router(e))
@@ -80,10 +109,54 @@ func TestCreateArticleWithoutCookie(t *testing.T) {
 
 	res, err := client.Do(req)
 	if err != nil {
-		t.Fatalf("http.Put failed: %s", err)
+		t.Fatalf("http.Post failed: %s", err)
 	}
 
 	assert.Equal(t, http.StatusUnauthorized, res.StatusCode)
+}
+
+func TestCreateArticleWithUnknownType(t *testing.T) {
+	e := echo.New()
+	ts := httptest.NewServer(Router(e))
+	defer ts.Close()
+
+	jsonStr := `{"title":"タイトル","body":["ABC", "DEF"]}`
+	// http.NewRequestのの第3引数にはio.Readerを指定するため、バイト列を渡す
+	paramsJson := bytes.NewBuffer([]byte(jsonStr))
+	req, _ := http.NewRequest("POST", ts.URL+"/api/articles", paramsJson)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzI3Mjk3NjEsImlzcyI6InVzZXIifQ.9NlQvzKYioB2um6NvB_ZpdKf9og5nRDb9oUzNAjkohk;")
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("http.Post failed: %s", err)
+	}
+
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+}
+
+func TestCreateArticleWithoutTitle(t *testing.T) {
+	e := echo.New()
+	// アプリケーションサーバーの設定をテスト用サーバーでも設定しないと未定義で落ちる
+	e.Validator = &CustomValidator{Validator: validator.New()}
+	ts := httptest.NewServer(Router(e))
+	defer ts.Close()
+
+	jsonStr := `{"title":"","body":"ボディ"}`
+	// http.NewRequestのの第3引数にはio.Readerを指定するため、バイト列を渡す
+	paramsJson := bytes.NewBuffer([]byte(jsonStr))
+	req, _ := http.NewRequest("POST", ts.URL+"/api/articles", paramsJson)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzI3Mjk3NjEsImlzcyI6InVzZXIifQ.9NlQvzKYioB2um6NvB_ZpdKf9og5nRDb9oUzNAjkohk;")
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("http.Post failed: %s", err)
+	}
+
+	assert.Equal(t, http.StatusUnprocessableEntity, res.StatusCode)
 }
 
 func TestUpdateArticleWithoutCookie(t *testing.T) {
@@ -106,6 +179,50 @@ func TestUpdateArticleWithoutCookie(t *testing.T) {
 	assert.Equal(t, http.StatusUnauthorized, res.StatusCode)
 }
 
+func TestUpdateArticleWithUnknownType(t *testing.T) {
+	e := echo.New()
+	ts := httptest.NewServer(Router(e))
+	defer ts.Close()
+
+	jsonStr := `{"title":"タイトル","body":["ABC", "DEF"]}`
+	// http.NewRequestのの第3引数にはio.Readerを指定するため、バイト列を渡す
+	paramsJson := bytes.NewBuffer([]byte(jsonStr))
+	req, _ := http.NewRequest("PUT", ts.URL+"/api/articles/1", paramsJson)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzI3Mjk3NjEsImlzcyI6InVzZXIifQ.9NlQvzKYioB2um6NvB_ZpdKf9og5nRDb9oUzNAjkohk;")
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("http.Put failed: %s", err)
+	}
+
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+}
+
+func TestUpdateArticleWithoutTitle(t *testing.T) {
+	e := echo.New()
+	// アプリケーションサーバーの設定をテスト用サーバーでも設定しないと未定義で落ちる
+	e.Validator = &CustomValidator{Validator: validator.New()}
+	ts := httptest.NewServer(Router(e))
+	defer ts.Close()
+
+	jsonStr := `{"title":"","body":"ボディ"}`
+	// http.NewRequestのの第3引数にはio.Readerを指定するため、バイト列を渡す
+	paramsJson := bytes.NewBuffer([]byte(jsonStr))
+	req, _ := http.NewRequest("PUT", ts.URL+"/api/articles/1", paramsJson)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzI3Mjk3NjEsImlzcyI6InVzZXIifQ.9NlQvzKYioB2um6NvB_ZpdKf9og5nRDb9oUzNAjkohk;")
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("http.Put failed: %s", err)
+	}
+
+	assert.Equal(t, http.StatusUnprocessableEntity, res.StatusCode)
+}
+
 func TestDeleteArticleWithoutCookie(t *testing.T) {
 	e := echo.New()
 	ts := httptest.NewServer(Router(e))
@@ -122,61 +239,3 @@ func TestDeleteArticleWithoutCookie(t *testing.T) {
 
 	assert.Equal(t, http.StatusUnauthorized, res.StatusCode)
 }
-
-// func setDummyCookie(c echo.Context) error {
-// 	cookie := &http.Cookie{}
-// 	cookie.Name = "jwt"
-// 	dummyToken, _ := util.GenerateJwtToken("dummy")
-// 	cookie.Value = dummyToken
-// 	cookie.Expires = time.Now().Add(24 * time.Hour)
-// 	c.SetCookie(cookie)
-// 	return nil
-// }
-
-// func TestPutUnknownType(t *testing.T) {
-// 	e := echo.New()
-// 	ts := httptest.NewServer(Router(e))
-// 	defer ts.Close()
-
-// 	jsonStr := `{"title":"タイトル","body":["ABC", "DEF"]}`
-// 	// http.NewRequestのの第3引数にはio.Readerを指定するため、バイト列を渡す
-// 	paramsJson := bytes.NewBuffer([]byte(jsonStr))
-// 	req, _ := http.NewRequest("PUT", ts.URL+"/api/articles/1", paramsJson)
-// 	if err := setDummyCookie(e.NewContext(req, httptest.NewRecorder())); err != nil {
-// 		t.Fatalf("setDummyCookie failed: %s", err)
-// 	}
-// 	req.Header.Set("Content-Type", "application/json")
-// 	client := &http.Client{}
-
-// 	res, err := client.Do(req)
-// 	if err != nil {
-// 		t.Fatalf("http.Put failed: %s", err)
-// 	}
-
-// 	assert.Equal(t, http.StatusBadRequest, res.StatusCode)
-// }
-
-// func TestPutNonTitle(t *testing.T) {
-// 	e := echo.New()
-// 	// アプリケーションサーバーの設定をテスト用サーバーでも設定しないと未定義で落ちる
-// 	e.Validator = &CustomValidator{Validator: validator.New()}
-// 	ts := httptest.NewServer(Router(e))
-// 	defer ts.Close()
-
-// 	jsonStr := `{"title":"","body":"ボディ"}`
-// 	// http.NewRequestのの第3引数にはio.Readerを指定するため、バイト列を渡す
-// 	paramsJson := bytes.NewBuffer([]byte(jsonStr))
-// 	req, _ := http.NewRequest("PUT", ts.URL+"/api/articles/1", paramsJson)
-// 	if err := setDummyCookie(e.NewContext(req, httptest.NewRecorder())); err != nil {
-// 		t.Fatalf("setDummyCookie failed: %s", err)
-// 	}
-// 	req.Header.Set("Content-Type", "application/json")
-// 	client := &http.Client{}
-
-// 	res, err := client.Do(req)
-// 	if err != nil {
-// 		t.Fatalf("http.Put failed: %s", err)
-// 	}
-
-// 	assert.Equal(t, http.StatusUnprocessableEntity, res.StatusCode)
-// }
