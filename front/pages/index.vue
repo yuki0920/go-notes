5407b951761b6ea4b7b1f1bb3a040e9e336e2211
yuki0920 <ywsep20@gmail.com>
2021-09-27 09:14:12 +0900

    Add loading icon for index

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,26 +1,33 @@
 <template>
-  <div class="l-col">
+  <div class="l-col pt-2">
     <div class="d-flex flex-column">
       <h1>
         記事一覧
       </h1>
-      <article v-for="(article, index) in articles" :key="index">
-        <div class="p-2">
-          <nuxt-link :to="`/articles/${article.id}`">
-            <p class="h3">
-              {{ article.title }}
-            </p>
-          </nuxt-link>
-          <div>
-            <small class="text-muted">
-              {{ article.created }}
-            </small>
+      <template v-if="articles.length > 1">
+        <article v-for="(article, index) in articles" :key="index">
+          <div class="p-2">
+            <nuxt-link :to="`/articles/${article.id}`">
+              <p class="h3">
+                {{ article.title }}
+              </p>
+            </nuxt-link>
+            <div>
+              <small class="text-muted">
+                {{ article.created }}
+              </small>
+            </div>
           </div>
-        </div>
-      </article>
-      <button v-if="!finished" class="btn btn-dark" @click="load">
-        もっとみる
-      </button>
+        </article>
+        <button v-if="!finished" class="btn btn-dark" @click="load">
+          もっとみる
+        </button>
+      </template>
+      <div v-else class="d-flex justify-content-center align-items-center">
+        <div class="spinner-border mr-2" style="width: 2rem; height: 2rem;" role="status" />
+        <span>Loading...(Please wait 30 secs for heroku's server to start)</span>
+        <div class="spinner-border ml-2" style="width: 2rem; height: 2rem;" role="status" />
+      </div>
     </div>
   </div>
 </template>
