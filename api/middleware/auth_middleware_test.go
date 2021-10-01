3de2d1462136fcd4dae9eb4b007a21ad7589c876
yuki0920 <ywsep20@gmail.com>
2021-10-01 21:46:22 +0900

    Add test with cookie

--- api/middleware/auth_middleware_test.go
+++ api/middleware/auth_middleware_test.go
@@ -15,12 +15,27 @@ func TestIsAuthenticatedWithoutCookie(t *testing.T) {
     e := echo.New()
     req := httptest.NewRequest(echo.GET, "/", nil)
     res := httptest.NewRecorder()
-    c := e.NewContext(req, res)
+    context := e.NewContext(req, res)
     hundleFunc := middleware.IsAuthenticated(func(c echo.Context) error {
         return c.String(http.StatusOK, "OK")
     })
 
-    err := hundleFunc(c)
+    err := hundleFunc(context)
     require.NoError(t, err)
     assert.Equal(t, http.StatusUnauthorized, res.Code)
+}
+
+func TestIsAuthenticatedWithCookie(t *testing.T) {
+    e := echo.New()
+    req := httptest.NewRequest(echo.GET, "/", nil)
+	req.Header.Set("Cookie", "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ1c2VyIn0.l5OzH8D-jhBGpWOaTICi65_Njdgq78TV6t_z-5JymtQ;")
+    res := httptest.NewRecorder()
+    context := e.NewContext(req, res)
+    hundleFunc := middleware.IsAuthenticated(func(c echo.Context) error {
+        return c.String(http.StatusOK, "OK")
+    })
+
+    err := hundleFunc(context)
+    require.NoError(t, err)
+    assert.Equal(t, http.StatusOK, res.Code)
 }
