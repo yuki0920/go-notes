66c2d76efae6d63f834d708a47273f79680c3e2f
yuki0920 <ywsep20@gmail.com>
2021-10-06 15:01:28 +0900

    Add usecase

--- api/domain/repository/article_repository.go
+++ api/domain/repository/article_repository.go
@@ -6,4 +6,5 @@ type ArticleRepository interface {
 	GetById(id int) (*model.Article, error)
 	ListByCursor(cursor int) ([]*model.Article, error)
 	Create(article *model.Article) (int64, error)
+	Update(article *model.Article) error
 }
