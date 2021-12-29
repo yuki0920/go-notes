b3e394b58f13b5eee364093830819d372859e0cb
yuki0920 <ywsep20@gmail.com>
2021-12-29 16:03:18 +0900

    Fix model

--- api/domain/model/category.go
+++ api/domain/model/category.go
@@ -3,9 +3,8 @@ package model
 import "time"
 
 type Category struct {
-	ID           int                    `db:"id" json:"id"`
-	Title        string                 `db:"title" json:"title"`
-	Relationship []CategoryRelationship `json:"relationship"`
-	Created      time.Time              `db:"created" json:"created"`
-	Updated      time.Time              `db:"updated" json:"updated"`
+	ID      int       `db:"id" json:"id"`
+	Title   string    `db:"title" json:"title"`
+	Created time.Time `db:"created" json:"created"`
+	Updated time.Time `db:"updated" json:"updated"`
 }
